9e55770be849b91ec9d0416760a0de92d919b344
Lucas Sjögren <Locosiap41@gmail.com>
2023-12-06 20:49:39 +0100

    Added two ViewQueries

--- WebbShopManager/Program.cs
+++ WebbShopManager/Program.cs
@@ -1,11 +1,22 @@
 ﻿using WebbShopManager.Views;
+using WebbShopManager.Repositry;
+using WebbShopManager.Entities;
 class Program
 {
     static void Main(string[] args)
     {
         UserInterface userInterface = new UserInterface();
 
-        userInterface.ViewMainMenu();     
-        userInterface.ViewSearchMeny();
+        List<Advertisement> advertisements = AdvertisementRepo.SearchForAdvertisement();
+
+        Console.WriteLine("List of Advertisements:");
+        foreach (var ad in advertisements)
+        {
+            Console.WriteLine($"ID: {ad.AdvertisementID}, Title: {ad.Title}, Description: {ad.DescriptionColumn}, {ad.Price}");
+            // Add more properties as needed
+        }
+
+        //userInterface.ViewMainMenu();     
+        //userInterface.ViewSearchMeny();
     }
 }
--- WebbShopManager/Repositry/AdvertisementRepo.cs
+++ WebbShopManager/Repositry/AdvertisementRepo.cs
@@ -12,13 +12,25 @@ namespace WebbShopManager.Repositry
 {
     public static class AdvertisementRepo
     {
-        private static string _connString = "Data Source=DESKTOP-F2QGBSH\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=true;trustservercertificate=true";
+        private static string _connString = "Data Source=DESKTOP-F2QGBSH\\SQLEXPRESS;Initial Catalog=WebbShopAdvertisements;Integrated Security=true;trustservercertificate=true";
 
         public static List<Advertisement> GetAllAdvertisements()
         {
             using (IDbConnection db = new SqlConnection(_connString))
             {
                 return db.Query<Advertisement>("ShowAllAdvertisements",
+                    commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
+
+        public static List<Advertisement> SearchForAdvertisement()
+        {
+            using (IDbConnection db = new SqlConnection(_connString))
+            {
+                DynamicParameters dynamicParameters = new DynamicParameters(); 
+
+
+                return db.Query<Advertisement>("SearchForAdvertisementByName",
                     commandType: CommandType.StoredProcedure).ToList();
             }
         }
